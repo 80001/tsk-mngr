52221134ab758daca3c5ed68a3c8c5998f1e8ef4
Roma Herman <romik.herman@gmail.com>
2023-08-06 15:14:19 +0300

    Add Brands

--- vrstk/src/App.js
+++ vrstk/src/App.js
@@ -18,7 +18,6 @@ const App = () => {
                 <Navbar />
                 <Header />
             </div>
-            <Article />
             <Brand />
             <WhatGPT />
             <Features />
