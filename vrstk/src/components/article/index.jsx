8039ddfdb81604fa48976d50fecdc8d5cef91e13
Roma Herman <romik.herman@gmail.com>
2023-08-06 18:28:30 +0300

    Add blog

--- vrstk/src/components/article/index.jsx
+++ vrstk/src/components/article/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './styles.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
     return (
-        <div>Article</div>
+        <div className='gpt3__blog-container-article'>
+            <div className='gpt3__blog-container-article-image'>
+                <img src={imgUrl} alt="blog image" />
+            </div>
+            <div className='gpt3__blog-container-article-content'>
+
+                <div className='gpt3__blog-container-article-image'>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                    <p>Read Full Article</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
