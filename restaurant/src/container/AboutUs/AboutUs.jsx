48f777960180f845ae34deefde2b0756da98af46
Roma Herman <romik.herman@gmail.com>
2023-08-08 22:48:04 +0300

    Add Menu

--- restaurant/src/container/AboutUs/AboutUs.jsx
+++ restaurant/src/container/AboutUs/AboutUs.jsx
@@ -4,7 +4,8 @@ import "./AboutUs.css";
 import { SubHeading } from "../../components";
 
 const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding">
+  <div
+    className="app__aboutus app__bg flex__center section__padding" id="about">
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
