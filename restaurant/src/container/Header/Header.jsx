424a9ddb991a5df55792be4c7a57ad257b4492c9
Roma Herman <romik.herman@gmail.com>
2023-08-08 20:49:02 +0300

    Add AboutUs

--- restaurant/src/container/Header/Header.jsx
+++ restaurant/src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ import { SubHeading } from '../../components';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className="app__wrapper-info">
-      <SubHeading title='Title!' />
+      <SubHeading title='Chase The New Flavour' />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, officiis quod error vero culpa voluptas quisquam unde dolorem ducimus atque vitae, iusto libero odit facilis deleniti et eveniet alias. Labore. </p>
       <button type='button' className="custom__button">Explore Menu</button>
