749ea855d9edb45bed65d840ee06937e8eca727f
Roma Herman <romik.herman@gmail.com>
2023-08-12 20:52:08 +0300

    Add Business

--- hoobank/src/components/Business.jsx
+++ hoobank/src/components/Business.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { apple, bill, google } from '../assets'
 
 const Business = () => {
     return (
-        <div>Business</div>
+        <div className='business'>
+            <div className="business__left">
+                <img src={bill} alt="bill" />
+            </div>
+            <div className="business__right">
+                <h2>Easily control your billing & invoicing.</h2>
+                <p>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+                <div>
+                    <img src={apple} alt="apple" />
+                    <img src={google} alt="google" />
+                </div>
+            </div>
+        </div>
     )
 }
 
