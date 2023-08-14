6a20e0888112e13b6e67a2fe7e17c8d6eebe9bbf
Roma Herman <romik.herman@gmail.com>
2023-08-14 16:06:55 +0300

    Add CTA

--- hoobank/src/components/CTA.jsx
+++ hoobank/src/components/CTA.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const CTA = () => {
     return (
-        <div>CTA</div>
+        <div className='cta bg-black-gradient-2'>
+            <div>
+                <h2>Let's try our service now!</h2>
+                <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+            </div>
+            <button type="button" className="button">Get Sturted</button>
+        </div>
     )
 }
 
--- hoobank/src/components/Testimonials.jsx
+++ hoobank/src/components/Testimonials.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { clients } from '../constant'
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <div className='company'>
+            {clients.map(item => (
+                <img src={item.logo} alt={item.id} key={item.id} />
+            ))}
+        </div>
     )
 }
 
